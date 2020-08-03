aa6896b50168d9e30c32cd64351ded6bd826f7e2
Andrei <t.andrei2598@gmail.com>
2020-08-03 21:36:02 +0300

    Slidere

---
+++ Assets/Scripts/SceneSwap.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneSwap : MonoBehaviour
+{
+    private Button _button;
+
+    public int scene;
+
+    private void Start()
+    {
+        _button = gameObject.GetComponent<Button>();
+
+        if(_button == null)
+        {
+            Debug.Log("Scritul nu e pe buton.");
+        }
+
+        _button.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: scene);
+    }
+   
+}
