0cb1b0ad2fb41c60dec694007dec0e2d5262cfa6
Vandal25 <61746759+Vandal25@users.noreply.github.com>
2020-08-07 01:13:50 +0300

    Menu + Score

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+	public GameObject game;
+	public GameObject popupMenu;
+	
+	void Update()
+    {
+        if(Input.GetKeyDown (KeyCode.Escape)) 
+        {
+            if (!game.activeInHierarchy) 
+            {
+                PopUpMenuOn();
+            }
+            if (game.activeInHierarchy) 
+            {
+                PopUpMenuOff();   
+            }
+        } 
+     }
+	 
+	public void PlayButton()
+	{
+			Score.scoreInt = 0;
+			SceneManager.LoadScene(1);
+	}
+	
+	public void QuitButton()
+	{
+			Application.Quit();
+	}
+	
+	public void MainMenu()
+	{
+			SceneManager.LoadScene(0);
+	}
+	
+	public void PopUpMenuOn()
+	{
+			game.SetActive(true);
+			popupMenu.SetActive(false);
+	}
+	
+	public void PopUpMenuOff()
+	{
+			game.SetActive(false);
+			popupMenu.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+	public static int scoreInt = 0;
+	public Text scoreText;
+	public Text highText;
+	public static int scoreHigh;
+	
+	void Start()
+	{
+		scoreHigh = PlayerPrefs.GetInt("High Score", 0);
+		highText.text = scoreHigh.ToString();
+	}
+	
+    void Update()
+    {
+		scoreText.text = scoreInt.ToString();
+    }
+	
+	public static void HighScore()
+	{
+			if( scoreInt > PlayerPrefs.GetInt("High Score", 0))
+			{
+				PlayerPrefs.SetInt("High Score", scoreInt);	
+				scoreHigh = scoreInt;
+			}
+			
+	}
+	
+	
+}
