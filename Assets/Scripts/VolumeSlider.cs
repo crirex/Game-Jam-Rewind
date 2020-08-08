c855754a080184d3af698024d4d1f3d4ad82b5f0
Crirex <crirex5k@gmail.com>
2020-08-08 10:09:58 +0300

    Volume Slider

---
+++ Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (slider != null)
+        {
+            AudioListener.volume = slider.value;
+        }
+    }
+}
